c4b87b53bfbfd4fe594db1fb2285c4463c94ecdc
Guilherme Moretti <guilhermebmoretti@gmail.com>
2025-02-10 09:03:19 -0300

    Trying to reproduce it

--- packages/arbor-store/tests/utilities/detach.test.ts
+++ packages/arbor-store/tests/utilities/detach.test.ts
@@ -7,6 +7,7 @@ import {
 } from "../../src/errors"
 import { ArborNode } from "../../src/types"
 import { detach, unwrap } from "../../src/utilities"
+import { proxiable } from "../../src/decorators"
 
 describe("detach", () => {
   it("cannot detach the state tree's root node", () => {
@@ -73,5 +74,61 @@ describe("detach", () => {
     expect(todos.get("b")).toBeUndefined()
     expect(store.state.get("b")).toBeUndefined()
     expect(detachedTodo).toBe(unwrap(todo))
+  })
+
+  it("dettaching on a complex data structure", () => {
+    @proxiable
+    class Todo {
+      constructor(public id: number, public text: string) {}
+    }
+
+    @proxiable
+    class Epic {
+      constructor(
+        public id: number,
+        public name: string,
+        public todos: Todo[]
+      ) {}
+
+      add({ id, text }: Todo) {
+        this.todos.push(new Todo(id, text))
+      }
+
+      deleteTodoById(id: number) {
+        const todo = this.todos.find((todo) => todo.id === id)
+        if (todo) {
+          return detach(todo)
+        }
+      }
+    }
+
+    @proxiable
+    class Project {
+      constructor(
+        public id: number,
+        public name: string,
+        public epics: Epic[]
+      ) {}
+    }
+
+    @proxiable
+    class Namespace {
+      constructor(public projects: Project[]) {}
+    }
+
+    const { state } = new Arbor(new Namespace([]))
+    state.projects.push(new Project(1, "Project 1", []))
+
+    const epic = new Epic(1, "Epic 1", [])
+    state.projects[0].epics.push(epic)
+    state.projects[0].epics.push(new Epic(2, "Epic 2", []))
+
+    epic.add({ id: 1, text: "Walk the dog" })
+    epic.add({ id: 2, text: "Do the dishes" })
+
+    epic.deleteTodoById(2)
+
+    expect(state.projects[0].epics.length).toBe(2)
+    expect(epic.todos.length).toBe(1)
   })
 })
